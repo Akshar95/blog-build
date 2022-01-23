fbb08f69a0f579b98362c5fb643beff406fc2f9b
Akshar95 <patel.akshar295@gmail.com>
2022-01-23 18:04:37 +0000

    Added right side sign in and styling to header

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 function Header() {
   return (
-    <header>
+    <header className="flex justify-between p-5 max-w-7xl mx-auto">
       <div className="flex items-center space-x-5">
         <Link href="/">
           <img
@@ -22,10 +22,19 @@ function Header() {
         </div>
         {/* hidden means hidden on the mobile screen. 
         then to get the text and the logo in the same line use flex on the container div
-        items-center - centers them on the y-axis*/}
+        items-center - centers them on the y-axis
+        justify-between - will space the 2 children
+        mx-auto - to center it in the x-axis */}
       </div>
 
-      <div></div>
+      <div className=" flex items-center space-x-5 text-green-600">
+          <h3>
+              Sign In
+          </h3>
+          <h3 className="border px-4 py-1 rounded-full border-green-600">
+              Get started
+          </h3>
+      </div>
     </header>
   );
 }
