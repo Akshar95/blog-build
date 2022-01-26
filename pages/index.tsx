b364edcc246391eb6f11e967c5867b912e3c2de4
Akshar95 <patel.akshar295@gmail.com>
2022-01-26 22:28:07 +0000

    adding SSR

--- pages/index.tsx
+++ pages/index.tsx
@@ -29,6 +29,11 @@ export default function Home() {
           </h1>
         </div>
       </div>
+      {/* Posts */}
     </div>
   );
+}
+
+export const getServerSideProps = async () => {
+  const query =
 }
