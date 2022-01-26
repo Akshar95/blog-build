2bc8ae9eab00b0ea1ee23817fb110469972a3959
Akshar95 <patel.akshar295@gmail.com>
2022-01-26 21:21:42 +0000

    adding the tagline under the navigation

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 function Header() {
   return (
-    <header className="flex justify-between p-5 max-w-7xl mx-auto">
+    <header className="flex justify-between p-5 max-w-6xl mx-auto">
       <div className="flex items-center space-x-5">
         <Link href="/">
           <img
@@ -28,12 +28,10 @@ function Header() {
       </div>
 
       <div className=" flex items-center space-x-5 text-green-600">
-          <h3>
-              Sign In
-          </h3>
-          <h3 className="border px-4 py-1 rounded-full border-green-600">
-              Get started
-          </h3>
+        <h3>Sign In</h3>
+        <h3 className="border px-4 py-1 rounded-full border-green-600">
+          Get started
+        </h3>
       </div>
     </header>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -10,6 +10,25 @@ export default function Home() {
       </Head>
 
       <Header />
+      <div className="flex items-center justify-between max-w-6xl mx-auto py-10 lg:py-0 bg-indigo-200 border-b-8 border-indigo-300">
+        <div className="px-10 space-y-5">
+          <h1 className="text-6xl max-w-xl font-serif">
+            <span className="underline decoration-black decoration-4">
+              MIST
+            </span>{" "}
+            is a blog that reviews
+          </h1>
+          <h2>
+            Read our reviews in order to clear the mist. The MISTery lies
+            within...
+          </h2>
+        </div>
+        <div>
+          <h1 className="hidden md:inline-flex text-14xl lg:text-21xl px-10">
+            M
+          </h1>
+        </div>
+      </div>
     </div>
   );
 }
